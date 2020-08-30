ab678fbf16fc1be18de6421f04409b5fad8ce611
Wes Hampton <wespipkin@gmail.com>
2020-08-30 13:01:44 -0500

    initial setup, adds form and todos list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Todos from './components/Todos';
+import Form from './components/Form';
+
+
 import './App.css';
 
 function App() {
+  const [text, setText] = useState([]);
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todos />
+      <Form />
     </div>
   );
 }
---
+++ src/components/Todos.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Todos = () => {
+  return (
+    <div className="container">
+      <header>Hello from Todos.js</header>
+      <div class="todo-container">
+        <ul class="todo-list"></ul>
+      </div>
+    </div>
+  );
+};
+
+export default Todos;
